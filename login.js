997db1fe8d0b1b2bdd34df81c564b7ee14554372
Kauã <kauaaparecidosilvamorais@gmail.com>
2026-06-23 18:00:52 -0300

    Criado uma pagina para fazer login, ainda falta adicionar umas coisas

---
+++ login.js
@@ -0,0 +1,46 @@
+document.getElementById("loginForm").addEventListener("submit", async function(event){
+
+    event.preventDefault();
+
+    const usuarioDigitado = document.getElementById("usuario").value;
+    const senhaDigitada = document.getElementById("senha").value;
+    const mensagem = document.getElementById("mensagem");
+
+    try {
+
+        const resposta = await fetch("usuarios.json");
+        const dados = await resposta.json();
+
+        const usuarioEncontrado = dados.usuarios.find(
+            user => user.usuario === usuarioDigitado
+        );
+
+        if(!usuarioEncontrado){
+            mensagem.style.color = "yellow";
+            mensagem.textContent = "Usuário não encontrado.";
+            return;
+        }
+
+        if(usuarioEncontrado.senha !== senhaDigitada){
+            mensagem.style.color = "#ff4d4d";
+            mensagem.textContent = "Senha incorreta.";
+            return;
+        }
+
+        mensagem.style.color = "#7CFC00";
+        mensagem.textContent = "Login realizado com sucesso!";
+        window.location.href = "index.html";
+
+
+        // Redirecionar para outra página
+        // window.location.href = "home.html";
+
+    } catch(erro){
+
+        mensagem.style.color = "white";
+        mensagem.textContent = "Erro ao carregar usuários.";
+
+        console.error(erro);
+    }
+
+});
